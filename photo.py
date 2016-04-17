575738c48b390346a4d8023d0bc6ad340eba2cba
Mohit Agrawal <mohit.agrawal886@gmail.com>
2016-04-17 18:12:19 +0000

    Use of photo sensor with pi.

---
+++ photo.py
@@ -0,0 +1,20 @@
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+def RCtime (RCpin):
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
+
+    GPIO.setup(RCpin, GPIO.IN)
+    while (GPIO.input(RCpin) == GPIO.LOW):
+        reading +=1
+
+    return reading
+while True:
+    print RCtime(11)
+
